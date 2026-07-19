767c821f954d13f02fe08039c1564cfd954b4efc
ainnabila928-droid <ainnabila928@gmail.com>
2026-07-20 00:07:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -132,12 +132,5 @@ function completeUnit(){
 }
 if("serviceWorker" in navigator)window.addEventListener("load",()=>navigator.serviceWorker.register("sw.js"));
 render();
-button {
-  letter-spacing: 1px;
-}
-
-.small {
-  line-height: 1.6;
-}
 
 
